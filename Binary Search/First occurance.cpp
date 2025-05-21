23ef47a35d0868dcb06be00c38054925632a98d4
GOKUL-S-2006 <gokul.sathiyamurthi2006@gmail.com>
2025-05-21 14:45:27 +0530

    Create First occurance.cpp

---
+++ Binary Search/First occurance.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) 
+    {
+        int mid = 0;
+        int low = 0;
+      int ans=-1;
+        int high = nums.size()-1;
+        while(low<=high)
+        {
+            mid = low+(high-low)/2;//OVERFLOW PREVENTING
+            if(target==nums[mid])
+            {
+              ans=mid;
+              high=mid-1;
+            }
+                
+            
+            else
+                if(nums[mid]>target)
+                    high = mid-1;
+            else
+                low = mid+1;
+        }
+        return ans;
+    }
+};
