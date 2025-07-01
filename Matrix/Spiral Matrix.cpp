128976ed78ad87f6ceb76b802dc26ef3110d65b9
Gokul S <gokul.sathiyamurthi2006@gmail.com>
2025-07-01 11:08:53 +0530

    Create Spiral Matrix.cpp

---
+++ Matrix/Spiral Matrix.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<int> spiralOrder(vector<vector<int>>& matrix) {
+    vector<int> result;
+
+    if (matrix.empty()) return result;
+
+    int top = 0;
+    int bottom = matrix.size() - 1;
+    int left = 0;
+    int right = matrix[0].size() - 1;
+
+    while (top <= bottom && left <= right) {
+        // Traverse from left to right
+        for (int i = left; i <= right; i++)
+            result.push_back(matrix[top][i]);
+        top++;
+
+        // Traverse from top to bottom
+        for (int i = top; i <= bottom; i++)
+            result.push_back(matrix[i][right]);
+        right--;
+
+        // Traverse from right to left
+        if (top <= bottom) {
+            for (int i = right; i >= left; i--)
+                result.push_back(matrix[bottom][i]);
+            bottom--;
+        }
+
+        // Traverse from bottom to top
+        if (left <= right) {
+            for (int i = bottom; i >= top; i--)
+                result.push_back(matrix[i][left]);
+            left++;
+        }
+    }
+
+    return result;
+}
+
+int main() {
+    vector<vector<int>> matrix = {
+        {1,  2,  3,  4},
+        {5,  6,  7,  8},
+        {9, 10, 11, 12}
+    };
+
+    vector<int> spiral = spiralOrder(matrix);
+    for (int num : spiral)
+        cout << num << " ";
+    return 0;
+}
