5c15d376aa2333559f6ea5db0a131dbc1f16599d
Gokul S <gokul.sathiyamurthi2006@gmail.com>
2025-12-08 19:20:57 +0530

    Create 27. Remove Element.cpp

---
+++ Two Pointers & Sliding Window/27. Remove Element.cpp
@@ -0,0 +1,16 @@
+int removeElement(vector<int>& nums, int val) {
+    int k = 0;
+
+    for(int i = 0; i < nums.size(); i++) {
+        if(nums[i] != val) {
+            nums[k] = nums[i];
+            k++;
+        }
+    }
+    return k;
+}
+
+
+| Time  | O(n) |
+| ----- | ---- |
+| Space | O(1) |
