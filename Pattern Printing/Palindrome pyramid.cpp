2f4b7cf2b0259c5af227ae07624687121799ca2d
Gokul S <gokul.sathiyamurthi2006@gmail.com>
2026-01-02 19:59:32 +0530

    Create Palindrome pyramid.cpp

---
+++ Pattern Printing/Palindrome pyramid.cpp
@@ -0,0 +1,35 @@
+ 1
+   212
+  32123
+ 4321234
+543212345
+
+
+
+  #include <iostream>
+using namespace std;
+
+int main() {
+    int n = 5;
+
+    for(int row = 1; row <= n; row++) {
+
+        // spaces
+        for(int col = 1; col <= n - row; col++) {
+            cout << " ";
+        }
+
+        // left part (row → 1)
+        for(int col = row; col >= 1; col--) {
+            cout << col;
+        }
+
+        // right part (2 → row)
+        for(int col = 2; col <= row; col++) {
+            cout << col;
+        }
+
+        cout << endl;
+    }
+    return 0;
+}
