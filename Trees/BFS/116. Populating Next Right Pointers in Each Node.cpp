1a4dbdb76d8f1087a13a6034ac2a51fe585fee6b
Gokul S <gokul.sathiyamurthi2006@gmail.com>
2026-05-12 19:10:26 +0530

    Update 116. Populating Next Right Pointers in Each Node.cpp

--- Trees/BFS/116. Populating Next Right Pointers in Each Node.cpp
+++ Trees/BFS/116. Populating Next Right Pointers in Each Node.cpp
@@ -113,4 +113,90 @@ public:
     }
 };
 
+
+Approach2:
+
+class Solution {
+public:
+
+    Node* connect(Node* root) {
+
+        if(root == NULL)
+            return NULL;
+
+        queue<Node*> q;
+        q.push(root);
+
+        while(!q.empty()) {
+
+            int size = q.size();
+
+            Node* prev = NULL;
+
+            for(int i = 0; i < size; i++) {
+
+                Node* curr = q.front();
+                q.pop();
+
+                if(prev != NULL) {
+                    prev->next = curr;
+                }
+
+                prev = curr;
+
+                if(curr->left)
+                    q.push(curr->left);
+
+                if(curr->right)
+                    q.push(curr->right);
+            }
+
+            prev->next = NULL;
+        }
+
+        return root;
+    }
+};
+
+Optimized:
+
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* left;
+    Node* right;
+    Node* next;
+
+    Node() : val(0), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val) : val(_val), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val, Node* _left, Node* _right, Node* _next)
+        : val(_val), left(_left), right(_right), next(_next) {}
+};
+*/
+
+class Solution {
+public:
+    Node* connect(Node* root) {
+        if(root==NULL) return root;
+        Node* leftmost=root;
+        while(leftmost->left!=nullptr){
+            Node* cur=leftmost;
+            while(cur!=nullptr){
+                cur->left->next=cur->right;
+                if(cur->next!=nullptr){
+                    cur->right->next=cur->next->left;
+                }
+                cur=cur->next;
+            }
+            leftmost=leftmost->left;
+        }
+        return root;
+    }
+};
+
+
   
