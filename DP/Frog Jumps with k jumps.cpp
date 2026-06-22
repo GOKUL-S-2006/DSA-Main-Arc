ba7c1201e8c74eb5fb91355cfce38512f5582f61
Gokul S <gokul.sathiyamurthi2006@gmail.com>
2026-06-22 20:05:14 +0530

    Update Frog Jumps with k jumps.cpp

--- DP/Frog Jumps with k jumps.cpp
+++ DP/Frog Jumps with k jumps.cpp
@@ -68,6 +68,36 @@ int main() {
 
 
 
+#include <bits/stdc++.h>
+using namespace std;
 
+int main() {
+    int n, k;
+    cin >> n >> k;
+
+    vector<int> height(n);
+    for (int i = 0; i < n; i++) {
+        cin >> height[i];
+    }
+
+    vector<int> dp(n, INT_MAX);
+    dp[0] = 0;
+
+    for (int i = 1; i < n; i++) {
+        for (int j = 1; j <= k; j++) {
+            if (i - j >= 0) {
+                dp[i] = min(dp[i], dp[i - j] + abs(height[i] - height[i - j]));
+            }
+        }
+    }
+
+    cout << dp[n - 1] << endl;
+
+    return 0;
+}
+
+Complexity
+Time: O(n * k)
+Space: O(n)
 
 
